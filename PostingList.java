fbc2fd729473054bc4350c883133f8c117ac06da
Julian Korell <julian.korell@reservix.de>
2016-02-05 17:04:46 +0100

    Initial commit

---
+++ PostingList.java
@@ -0,0 +1,40 @@
+// Copyright 2015, University of Freiburg,
+// Chair of Algorithms and Data Structures.
+// Hannah Bast <bast@cs.uni-freiburg.de>.
+
+import java.util.ArrayList;
+
+/**
+ * A posting list, with pairs doc id, score (both ints).
+ */
+public class PostingList {
+
+  /**
+   * Create from given ids and scores. Repeat given number of times with given
+   * offset for the repetitions.
+   */
+  public PostingList(ArrayList<Integer> ids, ArrayList<Integer> scores,
+    int numRepeats, int offset) {
+    int n = ids.size();
+    this.ids = new int[n * numRepeats + 1];
+    this.scores = new int[n * numRepeats + 1];
+    // this.ids = new ArrayList<Integer>(n * numRepeats);
+    // this.scores = new ArrayList<Integer>(n * numRepeats);
+    for (int k = 0; k < numRepeats; k++) {
+      for (int i = 0; i < n; i++) {
+        this.ids[i + k * n] = ids.get(i) + k * offset;
+        this.scores[i + k * n] = scores.get(i);
+      }
+    }
+    this.ids[n * numRepeats] = Integer.MAX_VALUE;
+    this.scores[n * numRepeats] = Integer.MAX_VALUE;
+  }
+
+  /**
+   * Store as ArrayList internally.
+   */
+  public int ids[];
+  public int scores[];
+  // public ArrayList<Integer> ids;
+  // public ArrayList<Integer> scores;
+}
